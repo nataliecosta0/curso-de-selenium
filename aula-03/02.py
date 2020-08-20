567f9987b35d2b3478d7c4e6b79979bd496f4534
Natalie Costa <costa.natalie02@gmail.com>
2020-08-20 00:16:58 -0300

    Resolvendo problema inicial do segundo exercicio

--- aula-03/02.py
+++ aula-03/02.py
@@ -5,3 +5,26 @@ Crie um programa com selenium que:
 
 url: https://curso-python-selenium.netlify.app/exercicio_02.html
 """
+from selenium.webdriver import Firefox
+from time import sleep
+
+url = 'https://curso-python-selenium.netlify.app/exercicio_02.html'
+browser = Firefox()
+browser.get(url)
+sleep(7)
+
+how_to_play, expect_number_text = browser.find_elements_by_tag_name('p')
+expect_number = expect_number_text.text.split()
+expect_position_number = expect_number[2]
+
+click_here = browser.find_element_by_partial_link_text('clique')
+number_give_from_game = 0
+
+while expect_position_number != lala:
+	click_here.click()
+	number = browser.find_elements_by_tag_name('p')
+	for num in number:
+		number_give_from_game = num.text
+		if number_give_from_game==expect_position_number:
+			break
+    
