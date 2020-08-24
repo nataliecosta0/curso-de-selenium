384d39999d2b398097278effb209ff483c083981
Natalie Costa <costa.natalie02@gmail.com>
2020-08-23 22:23:46 -0300

    Criado git ignore e adicionado a segunda parte do exercício 01 da aula 04

--- aula-04/01.py
+++ aula-04/01.py
@@ -13,12 +13,12 @@ sleep(5)
 
 def find_start(browser, main, anchor):
 	"""
-	Encontra o link começar por aqui pela tag 'a'
+	Encontra o link começar por aqui pela tag 'a'.
 	
 	Args: 
-		browser: Instância do browser
-		main: Será procurado dentro da tag 'main'
-		anchor: Será procurando dentro da tag 'a'
+		browser: Instância do browser.
+		main: Será procurado dentro da tag 'main'.
+		anchor: Será procurando dentro da tag 'a'.
 
 	Return:
 		anchor: Retorna link encontrado dentro da tag 'a'
@@ -27,4 +27,26 @@ def find_start(browser, main, anchor):
 	anchor = main.find_element_by_tag_name(anchor)
 	return anchor
 
+def select_answer(browser, main, anchor, answer):
+	"""
+	Encontra o link com a resposta desejada dentro da atráves do atributo
+	attr.
+	
+	Args: 
+		browser: Instância do browser
+		main: Será procurado dentro da tag 'main'
+		anchor: Será procurando dentro da tag 'a'
+		answer: Resposta desejada
+
+	Return:
+		element: Retorna link encontrado dentro do atributo attr.
+	"""
+	main = browser.find_element_by_tag_name(main)
+	anchor = main.find_elements_by_tag_name(anchor)
+
+	for element in anchor:
+		if (element.get_attribute('attr')) == answer:
+			return element
+
 find_start(browser, 'main', 'a').click()
+select_answer(browser, 'main', 'a', "errado").click()
